523d62b7abcd769cb2b039ec8b9b57b40951e446
Sasha Czerniawski <sashacandoit@gmail.com>
2022-03-10 06:26:33 -0500

    js file added and only cried twice

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, jsonify, request, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 
@@ -10,3 +10,23 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+
+@app.route('/')
+def homepage():
+    """Show the game board"""
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template('index.html', board=board)
+
+@app.route('/check-word')
+def check_word():
+    """Check if word is in the dictionary"""
+
+    word = request.args['word']
+    board = session['board']
+    res = boggle_game.check_valid_word(board, word)
+
+    print(res)
+
+    return jsonify({"result":res})
---
+++ static/boggle.js
@@ -0,0 +1,63 @@
+class Game {
+    /* make a new game at this DOM id */
+
+    constructor(boardId) {
+        this.score = 0;
+        this.words = new Set();
+        this.board = $("#" + boardId)
+
+        $("#check-word", this.board).on("submit", this.handleSubmit.bind(this));
+    }
+
+    /* Show correctly guest word in list */
+    showWord(word) {
+        $("#words", this.board).append($("<li>", { text: word }));
+    }
+
+    /* Display Status Message */
+    showMessage(msg, cls) {
+        $(".msg-txt", this.board)
+            .text(msg).removeClass().addClass(`msg ${cls}`);
+    }
+
+    showScore() {
+        $("#score", this.board).text(this.score);
+    }
+
+    /* handle word form submit event - check if valid */
+    async handleSubmit(evt) {
+    evt.preventDefault();
+    const $word = $("#word-input", this.board);
+
+    let wordVal = $word.val();
+
+    // If input is empty, return
+    if (!wordVal) return;
+
+    // if word submitted has already been found on board, show message with the error class
+    if (this.words.has(wordVal)) {
+        this.showMessage(`Already found ${wordVal}`, "err");
+        return;
+    }
+
+    //Check server to see if word is valid
+    const res = await axios.get('/check-word', { params: { word: wordVal } });
+    console.log(res);
+
+    if (res.data.result === "not-word") {
+        this.showMessage(`${wordVal} is not a valid word`, "err");
+    }
+    else if (res.data.result === "not-on-board") {
+        this.showMessage(`${wordVal} is not on this board`, "err");
+    }
+    else {
+        this.showWord(wordVal);
+        this.words.add(wordVal);
+        this.showMessage(`Added ${wordVal}`, "ok");
+    }
+    $word.val("").focus();
+
+    }
+    
+}
+
